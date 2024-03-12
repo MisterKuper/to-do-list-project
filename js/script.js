e2a2656c71ff4a97aca505dd28a7e715d6de6c16
MisterKuper <102316501+MisterKuper@users.noreply.github.com>
2024-03-12 15:09:45 +0100

    Srtyling todo container and classname in js
    styling todo container and in js file rewritten class in trash button functions

--- js/script.js
+++ js/script.js
@@ -29,7 +29,7 @@ function addTodo(event) {
     // CHECK TRASH BUTTON
     const trashButton = document.createElement("button");
     trashButton.innerHTML = '<i class="fas fa-trash"></i>';
-    trashButton.classList.add("complete-btn");
+    trashButton.classList.add("trashe-btn");
     todoDiv.appendChild(trashButton)
 
     // APPEND TO LIST
